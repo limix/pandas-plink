98f93fdab958210982716196cde6bda834af1ac8
Danilo Horta <danilo.horta@gmail.com>
2016-12-04 23:14:47 +0000

    mais pontos

--- pandas_plink/_bed_read.py
+++ pandas_plink/_bed_read.py
@@ -4,12 +4,20 @@ from _bed_reader import ffi, lib
 
 
 def read_bed(filepath, nrows, ncols):
+    import sys
+    print("ponto 0.1");sys.stdout.flush()
     X = zeros((nrows, ncols), int)
+
+    print("ponto 0.2");sys.stdout.flush()
     ptr = ffi.cast("uint64_t *", X.ctypes.data)
 
+    print("ponto 0.3");sys.stdout.flush()
     lib.read_bed(filepath, nrows, ncols, ptr)
 
+    print("ponto 0.4");sys.stdout.flush()
     X = X.astype(float, copy=False)
+    print("ponto 0.5");sys.stdout.flush()
     X[X == 3] = nan
+    print("ponto 0.6");sys.stdout.flush()
 
     return X
