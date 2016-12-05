28790260615fb03047705bd16f5c88b06774ab1f
Danilo Horta <danilo.horta@gmail.com>
2016-12-05 00:28:32 +0000

    chunks

--- bed_reader_build.py
+++ bed_reader_build.py
@@ -2,42 +2,58 @@ from cffi import FFI
 
 ffibuilder = FFI()
 
-ffibuilder.cdef("void read_bed(char*, uint64_t, uint64_t, uint64_t*);")
+ffibuilder.cdef(r"""
+    void read_bed(char*, uint64_t, uint64_t, uint64_t*, uint64_t,
+                  void (*cb_iter)());
+    extern "Python" void cb_iter();
+""")
 
 ffibuilder.set_source("_bed_reader", r"""
     #include <stdio.h>
     #include <math.h>
 
-    static void read_bed(char *filepath, uint64_t nrows, uint64_t ncols, uint64_t *out)
+    #define MIN( a, b ) ( ( a > b) ? b : a )
+
+    static void read_bed(char *filepath, uint64_t nrows, uint64_t ncols,
+                         uint64_t *out, uint64_t nint, void (*cb_iter)())
     {
-        FILE* f = fopen(filepath, "rb");
-        fseek(f, 3, SEEK_SET);
-        uint64_t e, i, j;
+            FILE* f = fopen(filepath, "rb");
+            fseek(f, 3, SEEK_SET);
+            uint64_t e, i, j;
 
-        uint64_t linesize = (uint64_t) ceil(ncols / 4.0);
-        char* buff = malloc(linesize);
+            uint64_t linesize = (uint64_t) ceil(ncols / 4.0);
 
-        char b, b0, b1, p0, p1;
+            uint64_t row_chunk = ceil(nrows / ((double) nint));
+            char* buff = malloc(linesize * row_chunk);
 
-        for (i = 0; i < nrows; ++i)
-        {
-            e = fread(buff, linesize, 1, f);
-            for (j = 0; j < ncols; ++j)
+            char b, b0, b1, p0, p1;
+            uint64_t row_start = 0, row_end;
+
+            while (row_start < nrows)
             {
-                b = (buff[j/4] >> (2*(j%4))) & 3;
+                row_end = MIN(row_start + row_chunk, nrows);
+                e = fread(buff, linesize, row_end - row_start, f);
 
-                b0 = b & 1;
-                b1 = b >> 1;
+                for (i = row_start; i < row_end; ++i)
+                {
+                    for (j = 0; j < ncols; ++j)
+                    {
+                        b = (buff[j/4] >> (2*(j%4))) & 3;
 
-                p0 = b0 ^ b1;
-                p1 = (b0 | b1) & b0;
+                        b0 = b & 1;
+                        b1 = b >> 1;
 
-                out[i * ncols + j] = (p1 << 1) | p0;
+                        p0 = b0 ^ b1;
+                        p1 = (b0 | b1) & b0;
+
+                        out[i * ncols + j] = (p1 << 1) | p0;
+                    }
+                }
+                row_start = row_end;
             }
-        }
 
-        free(buff);
-        fclose(f);
+            free(buff);
+            fclose(f);
     }
 """)
 
--- pandas_plink/_bed_read.py
+++ pandas_plink/_bed_read.py
@@ -2,6 +2,10 @@ from numpy import nan, zeros
 
 from _bed_reader import ffi, lib
 
+@ffi.def_extern()
+def cb_iter():
+    pass
+
 
 def read_bed(filepath, nrows, ncols):
     import sys
@@ -12,7 +16,8 @@ def read_bed(filepath, nrows, ncols):
     ptr = ffi.cast("uint64_t *", X.ctypes.data)
 
     print("ponto 0.3");sys.stdout.flush()
-    lib.read_bed(filepath, nrows, ncols, ptr)
+    lib.read_bed(filepath, nrows, ncols, ptr, 10, lib.cb_iter)
+    # lib.read_bed(filepath, nrows, ncols, ptr)
 
     print("ponto 0.4");sys.stdout.flush()
     X = X.astype(float, copy=False)
