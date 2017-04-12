e4b28875ad8c4896a35f9399952fe4c5c6812b1c
Danilo Horta <danilo.horta@gmail.com>
2017-04-12 13:44:02 +0100

    Pypi password variable

--- travis/deploy_wheels.sh
+++ travis/deploy_wheels.sh
@@ -7,5 +7,5 @@ if ! [ -z ${DOCKER_IMAGE+x} ]; then
     ls wheelhouse/
     pip install twine
     twine upload ${TRAVIS_BUILD_DIR}/wheelhouse/pandas_plink*.whl \
-        -u dhorta -p PYPI_PASSWORD
+        -u dhorta -p $PYPI_PASSWORD
 fi
