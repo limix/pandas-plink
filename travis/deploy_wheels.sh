ec514f356d9b70837018a6682369ad9ec9eea50c
Danilo Horta <danilo.horta@gmail.com>
2017-04-13 18:58:35 +0100

    Dist

--- travis/deploy_wheels.sh
+++ travis/deploy_wheels.sh
@@ -13,7 +13,7 @@ else
     if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
         ls dist
         pip install twine
-        twine upload ${TRAVIS_BUILD_DIR}/pandas_plink*.whl \
+        twine upload ${TRAVIS_BUILD_DIR}/dist/pandas_plink*.whl \
               -u dhorta -p ${PYPI_PASSWORD}
     fi
 fi
