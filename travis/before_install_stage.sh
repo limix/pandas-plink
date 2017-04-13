586b298894bb4dde9303879b393595554593721f
Danilo Horta <danilo.horta@gmail.com>
2017-04-13 13:23:57 +0100

    Osx pip

--- travis/before_install_stage.sh
+++ travis/before_install_stage.sh
@@ -2,13 +2,5 @@
 set -e -x
 
 if [ -z ${DOCKER_IMAGE+x} ]; then
-    if [ "${NUMBA}" == "true" ]; then
-        travis/install_clang38.sh;
-        travis/install_llvmlite.sh;
-    fi
     travis/install_pandoc.sh
-fi
-
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    brew install python3 pandoc libffi
 fi
--- travis/install_pandoc.sh
+++ travis/install_pandoc.sh
@@ -2,5 +2,9 @@
 
 set -e -x
 
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    brew install python3 pandoc libffi pip
+fi
+
 pip install pypandoc
 python -c "from pypandoc import download_pandoc as dp; dp(targetfolder='~/bin/');"
