3f3214d43d432803badb7a06ad2845b1cad2ec62
Danilo Horta <danilo.horta@gmail.com>
2017-04-13 16:41:33 +0100

    no pip sudo

--- travis/deploy_wheels.sh
+++ travis/deploy_wheels.sh
@@ -12,7 +12,7 @@ if ! [ -z ${DOCKER_IMAGE+x} ]; then
 else
     if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
         ls dist
-        sudo pip install twine
+        pip install twine
         twine upload ${TRAVIS_BUILD_DIR}/pandas_plink*.whl \
               -u dhorta -p ${PYPI_PASSWORD}
     fi
