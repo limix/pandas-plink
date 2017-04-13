a80ef14accbb5f02d4f4b98249c378f9f18ceabb
Danilo Horta <danilo.horta@gmail.com>
2017-04-13 16:40:55 +0100

    No old xcod

--- travis/install_pandoc.sh
+++ travis/install_pandoc.sh
@@ -4,9 +4,7 @@ set -e -x
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install python3 pandoc libffi
-    wget https://bootstrap.pypa.io/get-pip.py
-    sudo python get-pip.py
 fi
 
-sudo pip install pypandoc
-sudo python -c "from pypandoc import download_pandoc as dp; dp(targetfolder='~/bin/');"
+pip install pypandoc
+python -c "from pypandoc import download_pandoc as dp; dp(targetfolder='~/bin/');"
