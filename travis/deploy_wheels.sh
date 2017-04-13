21bacc993449b152fa4159e1b0beb2287c63e103
Danilo Horta <danilo.horta@gmail.com>
2017-04-13 15:45:39 +0100

    Deploy OSX

--- travis/deploy_wheels.sh
+++ travis/deploy_wheels.sh
@@ -3,14 +3,15 @@
 set -e -x
 
 if ! [ -z ${DOCKER_IMAGE+x} ]; then
-    if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-        docker run -e PYPI_PASSWORD=${PYPI_PASSWORD} --rm -v `pwd`:/io $DOCKER_IMAGE /bin/bash
-        ls
-        ls wheelhouse/
-        pip install twine
-        twine upload ${TRAVIS_BUILD_DIR}/wheelhouse/pandas_plink*.whl \
-              -u dhorta -p ${PYPI_PASSWORD}
-    else
+    docker run -e PYPI_PASSWORD=${PYPI_PASSWORD} --rm -v `pwd`:/io $DOCKER_IMAGE /bin/bash
+    ls
+    ls wheelhouse/
+    pip install twine
+    twine upload ${TRAVIS_BUILD_DIR}/wheelhouse/pandas_plink*.whl \
+          -u dhorta -p ${PYPI_PASSWORD}
+    fi
+else
+    if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
         ls dist
         pip install twine
         twine upload ${TRAVIS_BUILD_DIR}/pandas_plink*.whl \
