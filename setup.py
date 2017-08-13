ae53337a335b1d398dfb17807e9b5d03471d560d
Danilo Horta <danilo.horta@gmail.com>
2017-08-13 23:22:23 +0100

    1.2.5

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def setup_package():
 
     metadata = dict(
         name='pandas-plink',
-        version='1.2.4',
+        version='1.2.5',
         maintainer="Danilo Horta",
         maintainer_email="horta@ebi.ac.uk",
         description="Read PLINK files into Pandas data frames.",
