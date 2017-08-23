0b68011f9dfa74ab2fcf30bbb1b2f70f3f959d66
Danilo Horta <danilo.horta@gmail.com>
2017-08-23 15:32:42 +0100

    IOError

--- pandas_plink/test/test_reader.py
+++ pandas_plink/test/test_reader.py
@@ -41,5 +41,5 @@ def test_read_plink_prefix_dot():
     datafiles = join(dirname(realpath(__file__)), 'data_files')
     file_prefix = join(datafiles, 'data')
 
-    with pytest.raises(FileNotFoundError):
+    with pytest.raises(IOError):
         (bim, fam, bed) = read_plink('/tmp/84759873.genotypes.norm.renamed')
