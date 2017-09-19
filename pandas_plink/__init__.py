c8940a4b002e36bb9001397cce3d504cb45db5d2
Danilo Horta <danilo.horta@gmail.com>
2017-09-19 22:38:31 +0100

    1.2.14

--- pandas_plink/__init__.py
+++ pandas_plink/__init__.py
@@ -1,12 +1,12 @@
 from __future__ import absolute_import as _
 
-import _cffi_backend
+import _cffi_backend as _
 
 from ._test import test
 from .read import read_plink
 
 __name__ = "pandas-plink"
-__version__ = "1.2.13"
+__version__ = "1.2.14"
 __author__ = "Danilo Horta"
 __author_email__ = "horta@ebi.ac.uk"
 
