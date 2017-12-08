08b0f7e53f1e7208c0b0af8e3c1636b8579a2dd3
Danilo Horta <danilo.horta@gmail.com>
2017-12-08 02:27:43 +0000

    sane conftest.py

--- conftest.py
+++ conftest.py
@@ -1,7 +1,1 @@
-def get_pkg_name():
-    from setuptools import find_packages
-
-    return find_packages()[0]
-
-
-collect_ignore = ["setup.py", "{}/testit.py".format(get_pkg_name())]
+collect_ignore = ["setup.py"]
