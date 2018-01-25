dc6ea828643c48874e663b6c9019024d117fc1f9
Danilo Horta <danilo.horta@gmail.com>
2018-01-25 00:07:08 +0000

    Download exec travis appveyor

---
+++ version.py
@@ -0,0 +1,17 @@
+import re
+from os.path import join
+
+from setuptools import find_packages
+
+
+def get():
+    pkgnames = find_packages()
+    if len(pkgnames) == 0:
+        return "unknown"
+    pkgname = pkgnames[0]
+    content = open(join(pkgname, '__init__.py')).read()
+    c = re.compile(r"__version__ *= *('[^']+'|\"[^\"]+\")")
+    m = c.search(content)
+    if m is None:
+        return "unknown"
+    return m.groups()[0][1:-1]
