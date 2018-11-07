72d800d3044ec3666c4bdf209750ca4dd12ebbba
Danilo Horta <danilo.horta@gmail.com>
2018-11-07 12:32:21 +0000

    wildcard now working|

--- pandas_plink/test/test_reader.py
+++ pandas_plink/test/test_reader.py
@@ -16,24 +16,46 @@ def _ascii_airlock(v):
 
 def test_read_plink():
 
-    datafiles = join(dirname(realpath(__file__)), 'data_files')
-    file_prefix = join(datafiles, 'data')
+    datafiles = join(dirname(realpath(__file__)), "data_files")
+    file_prefix = join(datafiles, "data")
 
-    (bim, fam, bed) = read_plink(file_prefix)
+    (bim, fam, bed) = read_plink(file_prefix, verbose=False)
 
-    assert_array_equal(
-        bim.query("chrom=='1' and pos==72515")['snp'], ['rs4030300'])
+    assert_array_equal(bim.query("chrom=='1' and pos==72515")["snp"], ["rs4030300"])
     assert_array_equal(bim.query("chrom=='1'").shape, [10, 7])
     assert_array_equal(
-        fam.query("fid=='Sample_2' and iid=='Sample_2'")["trait"], ['-9'])
+        fam.query("fid=='Sample_2' and iid=='Sample_2'")["trait"], ["-9"]
+    )
 
-    assert_array_equal(bed,
-                       array([[2, 2, 1], [2, 1, 2], [nan, nan, nan],
-                              [nan, nan, 1], [2, 2, 2], [2, 2, 2], [2, 1, 0],
-                              [2, 2, 2], [1, 2, 2], [2, 1, 2]]))
+    assert_array_equal(
+        bed,
+        array(
+            [
+                [2, 2, 1],
+                [2, 1, 2],
+                [nan, nan, nan],
+                [nan, nan, 1],
+                [2, 2, 2],
+                [2, 2, 2],
+                [2, 1, 0],
+                [2, 2, 2],
+                [1, 2, 2],
+                [2, 1, 2],
+            ]
+        ),
+    )
 
 
 def test_read_plink_prefix_dot():
 
     with pytest.raises(IOError):
-        read_plink('/home/joao/84757.genotypes.norm.renamed')
+        read_plink("/home/joao/84757.genotypes.norm.renamed")
+
+
+def test_read_plink_wildcard():
+    datafiles = join(dirname(realpath(__file__)), "data_files")
+    file_prefix = join(datafiles, "chr*")
+
+    (bim, fam, bed) = read_plink(file_prefix, verbose=False)
+    assert_array_equal(bim[bim["chrom"] == "11"]["i"].values[:2], [0, 1])
+    assert_array_equal(bim[bim["chrom"] == "12"]["i"].values[:2], [779, 780])
