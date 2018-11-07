87564a04799b5fa27b7356f30fbeee6d6970ff0d
Danilo Horta <danilo.horta@gmail.com>
2018-11-07 12:50:31 +0000

    1.2.29

--- pandas_plink/__init__.py
+++ pandas_plink/__init__.py
@@ -3,16 +3,15 @@ from __future__ import absolute_import as _
 from ._read import read_plink
 from ._testit import test
 
-__version__ = "1.2.28"
+__version__ = "1.2.29"
 
 
 def example_file_prefix():
     r"""Data files prefix."""
     import os
-    p = __import__('pandas_plink').__path__[0]
-    return os.path.join(p, 'test', 'data_files', 'data')
+
+    p = __import__("pandas_plink").__path__[0]
+    return os.path.join(p, "test", "data_files", "data")
 
 
-__all__ = [
-    "__version__", "test", 'example_file_prefix', 'read_plink'
-]
+__all__ = ["__version__", "test", "example_file_prefix", "read_plink"]
