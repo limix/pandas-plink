e2f875757bb94ab356288f680441576c338c11f6
Danilo Horta <danilo.horta@gmail.com>
2018-12-13 18:23:46 -0200

    format

--- version.py
+++ version.py
@@ -9,7 +9,7 @@ def get():
     if len(pkgnames) == 0:
         return "unknown"
     pkgname = pkgnames[0]
-    content = open(join(pkgname, '__init__.py')).read()
+    content = open(join(pkgname, "__init__.py")).read()
     c = re.compile(r"__version__ *= *('[^']+'|\"[^\"]+\")")
     m = c.search(content)
     if m is None:
